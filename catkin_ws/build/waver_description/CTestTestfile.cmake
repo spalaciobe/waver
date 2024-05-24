f3b9cc1379a7ba9019b17273b6b26b6afa672969
GGomezMorales <gugomog3@gmail.com>
2024-05-24 12:25:45 -0500

    chore: deleted catkin workspace

--- catkin_ws/build/waver_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /catkin_ws/src/waver_description
-# Build directory: /catkin_ws/build/waver_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
