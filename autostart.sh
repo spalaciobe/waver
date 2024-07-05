9824694d303a466873d63d7d26c5b6b492298974
GGomezMorales <gugomog3@gmail.com>
2024-07-04 21:48:41 -0500

    chore: added amcl node

--- autostart.sh
+++ autostart.sh
@@ -10,8 +10,6 @@ if [ $1 == "rviz" ]; then
     roslaunch waver_viz rviz.launch
 elif [ $1 == "gz" ]; then
     roslaunch waver_gazebo gazebo.launch
-elif [ $1 == "controller" ]; then
-    roslaunch waver_description controller.launch
 elif [ $1 == "teleop" ]; then
     rosrun teleop_twist_keyboard teleop_twist_keyboard.py
 fi
