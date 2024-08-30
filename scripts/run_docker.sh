1e90e4dbbd3cc3aba98111835e5cf2c471aae64f
ggomezmorales <gugomog3@gmail.com>
2024-08-29 21:17:15 -0500

    chore: docker run updated

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -3,16 +3,15 @@
 PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
 source ${PROJECT_ROOT}/config.sh
 
-docker run --privileged --rm -it \
+docker run --privileged --rm -it --gpus all \
     --name $CONTAINER_NAME \
     -e DISPLAY \
     -e TERM \
     -e QT_X11_NO_MITSHM=1 \
     -e XAUTHORITY \
-    -v /etc/localtime:/etc/localtime:ro \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $XAUTHORITY:$XAUTHORITY \
-    -v ${PROJECT_ROOT}:/waver_ws/src \
+    --mount type=bind,source=${PROJECT_ROOT},target=/waver_ws/src \
     --net=host \
     -t \
     ${DOCKER_IMAGE_NAME} 
