33281b42ffb854c6e4d173f7e663226b53a7bf80
ggomezmorales <gugomog3@gmail.com>
2024-09-03 13:31:10 -0500

    feat: multiplatform implementation

--- scripts/run_nvidia.sh
+++ scripts/run_nvidia.sh
@@ -8,5 +8,5 @@ source ${PROJECT_ROOT}/config.sh
 rocker --x11 \
     --nvidia \
     --name=${CONTAINER_NAME} \
-    --volume="${PROJECT_ROOT}:/waver_ws/src" \
+    --volume="${PROJECT_ROOT}:/${WS_ROS}/src" \
     -- ${DOCKER_IMAGE_NAME}
